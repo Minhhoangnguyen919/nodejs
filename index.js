502416965a0ff112932573de15d01f78c4de1bac
Minhhoangnguyen919 <minhhoangpro09@gmail.com>
2021-12-31 00:55:51 +0700

    fix view pug

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const pug = require('pug');
+const app = express();
+const port = 3000;
+
+//HHTP loger
+app.use(morgan("combined"));
+app.use(express.static("public"));
+//template enginer
+app.set('index', './views');
+app.set('view engine', 'pug');
 
 //route
-app.get('/trang-chu',(req, res) => {
-    var a = 1
-    var b = 2
-    var c = a + b
-    res.send("hello world")
-})
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
-  })
+  });
